8de61c212c1cdcee43d501b164fb9e905d504f83
张婷 <343185404@qq.com>
2020-08-01 20:03:53 +0800

    主页静态页面

---
+++ src/components/search/index.vue
@@ -0,0 +1,22 @@
+<template>
+<view class="s-search">
+   <input class="s-input" auto-focus placeholder="搜索"/>
+</view>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'>
+  .s-search{
+    padding: 20rpx 16rpx;
+    background-color: #ff2d4a;
+    .s-input{
+      background: #fff;
+      padding: 0 10rpx;
+      height: 60rpx;
+      border-radius: 5rpx;
+    }
+  }
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,9 +1,131 @@
 <template>
-  <div class='container'>123</div>
+  <view class="h-home">
+    <Search />
+    <view class="h-home-swiper">
+      <swiper
+        indicator-dots
+        indicator-color="rgba(253,253,253,0.5)"
+        indicator-active-color="#fff"
+        autoplay
+        interval="3000"
+      >
+        <block v-for="item in 3" :key="item">
+          <swiper-item>
+            <navigator>
+              <image :src="`/static/uploads/banner${item+1}.png`" />
+            </navigator>
+          </swiper-item>
+        </block>
+      </swiper>
+    </view>
+      <view class="h-home-category">
+        <view class="category-item" v-for="item in 4" :key="item">
+          <view></view>
+          <text>化妆品</text>
+        </view>
+      </view>
+    <view v-for="item in 4" :key="item">
+      <view class="h-home-navigat1"></view>
+      <view class="h-home-navigat1_1 clearfix">
+        <view class="navigat1_1-item" v-for="item in 5" :key="item">
+          <navigator>
+            <image :src="`/static/uploads/pic_floor01_${item+1}@2x.png`" />
+          </navigator>
+        </view>
+      </view>
+    </view>
+    <view class="h-home-top">top</view>
+  </view>
 </template>
 
 <script>
-export default {}
+import Search from '@/components/search'
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    Search
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.h-home-top{
+  position: fixed;
+  bottom: 10rpx;
+  right: 10rpx;
+  width: 100rpx;
+  height: 100rpx;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 100rpx;
+  background: rgb(218, 211, 211);
+  color: rgb(83, 80, 80);
+  z-index: 9999;
+}
+.h-home-navigat1_1 {
+  padding: 16rpx;
+  .navigat1_1-item {
+    float: left;
+    &:first-child image {
+      vertical-align: middle;
+      width: 232rpx;
+      height: 386rpx;
+    }
+    &:nth-child(n + 2) image {
+      vertical-align: middle;
+      width: 233rpx;
+      height: 188rpx;
+    }
+    &:nth-child(2) image,
+    &:nth-child(4) image {
+      margin: 0 10rpx;
+    }
+    &:nth-child(2) image,
+    &:nth-child(3) image {
+      margin-bottom:10rpx;
+    }
+  }
+}
+.h-home-navigat1 {
+  height: 100rpx;
+  padding-top: 40rpx;
+  background: #f4f4f4 url("/static/uploads/pic_floor01_title.png") no-repeat
+    left/cover;
+  background-origin: content-box;
+  box-sizing: border-box;
+}
+.h-home-swiper {
+  image {
+    width: 100%;
+    height: 340rpx;
+  }
+}
+.h-home-category {
+  height: 180rpx;
+  padding: 24rpx 0;
+  display: flex;
+  .category-item {
+    width: 25%;
+    text-align: center;
+    & > text {
+      display: block;
+      margin-top: 10rpx;
+    }
+    & > view {
+      width: 128rpx;
+      height: 128rpx;
+      margin: auto;
+      border-radius: 50%;
+    }
+    &:nth-child(2n) > view {
+      background-color: pink;
+    }
+    &:nth-child(2n-1) > view {
+      background-color: rgb(102, 185, 233);
+    }
+  }
+}
+</style>
